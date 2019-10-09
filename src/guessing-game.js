11636d7c77eec78b80c406981daba8a2e34fa96d
bol2n <lukyanoff@tut.by>
2019-10-10 02:52:34 +0300

    feat: add task Guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        
     }
 
-    guess() {
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }     
 
+    guess() {
+        return Math.round((this.min + this.max)/2)
+        console.log("otvet"+this.guess)
     }
 
     lower() {
-
+        this.max = Math.round((this.min + this.max)/2)
+        Math.round((this.min + this.max)/2)
+        console.log("test lower"+this.max)
     }
 
     greater() {
-
+        this.min = Math.round((this.min + this.max)/2)
+        Math.round((this.min + this.max)/2)
+        console.log("test greater"+this.min)
     }
 }
+
 
 module.exports = GuessingGame;
